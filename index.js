19a611e41b1eb40e293ae8cc82a18f882174f2eb
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2025-06-06 08:47:48 +0530

    feat(api): implement authentication middleware

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("lakshman");
 });
-app.get("/user",(req,res)=> {
+app.get("/user", (req, res) => {
   res.send("lakshman naik");
 });
-app.get("/user/add",(req,res)=> {
-  const name=req.query.name;
-  const email=req.query.email;
+app.get("/user/add", (req, res) => {
+  const name = req.query.name;
+  const email = req.query.email;
   res.send(`user name is  ${name} and  mail ${email}`);
-  
 });
-app.get("/users",(req,res)=>{
-  res.send('<h1>users list</h1>');
+//my cusotm middle for authorization
+function checkMiddleware(req, res, next) {
+  if (req.query.token == "9949") {
+    next();
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+}
+app.get("/users",checkMiddleware, (req, res) => {
+  res.send("Authorized by Middleware");
 });
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port} `);
